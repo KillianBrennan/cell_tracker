b71647ef6df75670bbe438dfdc03122a9c102f26
Killian Brennan <kbrennan@env.ethz.ch>
2024-01-15 14:02:22 +0100

    replaced removed parts

--- cell_tracker.py
+++ cell_tracker.py
@@ -1946,6 +1946,39 @@ class Cell:
         }
         return cell_dict
 
+    def from_dict(self, cell_dict):
+        """
+        returns a cell object from a dictionary containing all cell object information
+
+        in
+        cell_dict: dictionary containing all cell object information, dict
+        """
+        self.cell_id = cell_dict["cell_id"]
+        self.parent = cell_dict["parent"]
+        self.child = cell_dict["child"]
+        self.merged_to = cell_dict["merged_to"]
+        self.died_of = cell_dict["died_of"]
+        self.lifespan = cell_dict["lifespan"]
+        self.datelist = [np.datetime64(t) for t in cell_dict["datelist"]]
+        self.lon = cell_dict["lon"]
+        self.lat = cell_dict["lat"]
+        self.mass_center_x = cell_dict["mass_center_x"]
+        self.mass_center_y = cell_dict["mass_center_y"]
+        self.max_x = cell_dict["max_x"]
+        self.max_y = cell_dict["max_y"]
+        self.delta_x = cell_dict["delta_x"]
+        self.delta_y = cell_dict["delta_y"]
+        self.area_gp = cell_dict["area_gp"]
+        self.max_val = cell_dict["max_val"]
+        self.score = cell_dict["score"]
+
+        self.overlap = [0] * len(self.datelist)
+        self.search_field = [None] * len(self.datelist)
+        self.search_vector = [[0, 0]] * len(self.datelist)
+        self.alive = False
+        self.field = None
+        self.label = []
+        self.swath = []
 
 def write_to_json(cellss, filename):
     """
@@ -2010,6 +2043,7 @@ def write_to_json(cellss, filename):
         json.dump(struct, f)
 
     return struct
+    
 
 
 def write_masks_to_netcdf(
@@ -2104,6 +2138,46 @@ def write_masks_to_netcdf(
 
     return ds
 
+def read_from_json(filename):
+    """
+    reads cell objects from json file
+    this is only a post processing function, not used in tracking
+    """
+
+    with open(filename, "r") as f:
+        struct = json.load(f)
+
+    if (
+        struct["data_structure"]
+        == "cell_data contains list of cells, each cell contains a a dictionary of cell parameters which are lists with length of the lifetime of the cell"
+    ):
+        cellss = []
+        for cell_dict in struct["cell_data"]:
+            cell = Cell(None, None, None)
+            cell.from_dict(cell_dict)
+            cellss.append(cell)
+
+    elif (
+        struct["data_structure"]
+        == "cell_data contains list of ensemle members, each of which is a list of cells, each cell contains a a dictionary of cell parameters which are lists with length of the lifetime of the cell"
+    ):
+        cellss = []
+        for member in struct["cell_data"]:
+            cells = []
+            for cell_dict in member:
+                cell = Cell(None, None, None)
+                cell.from_dict(cell_dict)
+                cells.append(cell)
+            cellss.append(cells)
+
+    elif struct["data_structure"] == "no cells found":
+        return []
+
+    else:
+        print("data structure not recognized")
+        return
+
+    return cellss
 
 if __name__ == "__main__":
     pass
